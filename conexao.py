30a5ef5cb9901f489694e20dc897087aea82cc19
Henrique1803 <henriquemateusteodoro@gmail.com>
2024-05-04 16:43:04 -0300

    sql básico para conectar com banco

--- conexao.py
+++ conexao.py
@@ -2,9 +2,33 @@ import psycopg2
 from config import user
 from config import senha
 
-conexao = psycopg2.connect(databe = "MAT_Analytics", 
-                           host = "localhost", 
-                           user = user, 
-                           password = senha, 
-                           porta = "5432")
-cursor = conexao.cursor()
+# Parâmetros de conexão com o banco de dados PostgreSQL
+dbname = 'MAT_Analytics'
+user = user
+password = senha
+host = 'localhost'
+port = '5432'
+
+try:
+    # Conectando ao banco de dados
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+    
+    # Criando um cursor para executar operações no banco de dados
+    cursor = conn.cursor()
+    
+    # Exemplo: Executando uma consulta SQL
+    cursor.execute("SELECT * FROM appliedon;")
+    
+    # Obtendo os resultados da consulta
+    rows = cursor.fetchall()
+    
+    # Imprimindo os resultados
+    for row in rows:
+        print(row)
+    
+    # Fechando o cursor e a conexão com o banco de dados
+    cursor.close()
+    conn.close()
+
+except psycopg2.Error as e:
+    print("Erro ao conectar ao banco de dados PostgreSQL:", e)
